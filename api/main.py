2fc96e27f8b6c89c361b9a5144390443c6fe9f9b
Dax Harris <matteovh@gmail.com>
2022-06-24 21:25:12 -0400

    api setup

--- api/main.py
+++ api/main.py
@@ -1,3 +1,36 @@
 from starlite import Starlite
+from util import ApplicationState
+import json
+import os
+from pymongo.mongo_client import MongoClient
+from keycloak import Keycloak
 
-app = Starlite(route_handlers=[])
+
+def load_app(state: ApplicationState):
+    if "CONSOLIDATOR_CONFIG" in os.environ.keys():
+        cfg_path = os.environ["CONSOLIDATOR_CONFIG"]
+    else:
+        cfg_path = "config.json"
+    with open(cfg_path, "r") as f:
+        state.config = json.load(f)
+
+    _client = MongoClient(
+        host=state.config["database"]["host"],
+        port=state.config["database"]["port"],
+        username=state.config["database"]["username"],
+        password=state.config["database"]["password"],
+    )
+
+    state.database = _client[state.config["database"]["database"]]
+
+    if "keycloak" in state.config["authSource"]["modes"]:
+        state.keycloak = Keycloak(
+            state.config["authSource"]["keycloak"]["url"],
+            state.config["authSource"]["keycloak"]["sso"],
+            state.config["authSource"]["keycloak"]["clientId"],
+            state.config["authSource"]["keycloak"]["clientSecret"],
+        )
+
+
+if __name__ == "__main__":
+    app = Starlite(route_handlers=[], on_startup=[load_app])
---
+++ api/util/application_state.py
@@ -0,0 +1,12 @@
+from typing import Any
+import pymongo.database
+from starlite.datastructures import State
+import typing
+from keycloak import Keycloak
+
+class ApplicationState(State):
+    def __init__(self, state: typing.Optional[typing.Dict[str, typing.Any]] = None):
+        super().__init__(state)
+        self.config: Any = None
+        self.database: pymongo.database.Database = None
+        self.keycloak: Keycloak = None
